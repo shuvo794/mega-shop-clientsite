ef7905f5e3284f086107488077a6e15168f653f0
shuvo794 <shuvohossain794@gmail.com>
2022-04-24 17:47:15 +0600

    added

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,61 +1,58 @@
 import React from 'react';
 import {
-	FooterLinkItems,
-	FooterLinkTitle,
-	FooterLink,
-	FooterLogo,
-
-	FooterRights,
-	FooterSocialIcon,
-	FooterWrapper,
-	FooterAddress,
-	FooterColumn,
-	FooterGrid,
-} from './FooterStyles';
-
-import { Row, Section } from '../../globalStyles';
-import { footerData, footerSocialData } from './../../data/FooterData';
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  FooterLogo,
+  FooterRights,
+  FooterSocialIcon,
+  FooterWrapper,
+  FooterAddress,
+  FooterColumn,
+  FooterGrid,
+} from "./FooterStyles";
+// import { footerData, footerSocialData } from '../../../src/data/FooterData';
+import { Row, Section } from "../../globalStyles";
+import { footerData, footerSocialData } from "./../../data/FooterData";
 const Footer = () => {
   return (
-    <Section padding="4rem 2 2rem 2">
-    <FooterWrapper>
-      <FooterGrid justify="space-between">
-        <FooterColumn id="footerLogo">
-          <FooterLogo to="/">
-           
-          Mega Shop
-          </FooterLogo>
-          <FooterAddress>
-           
-          </FooterAddress>
+    <Section padding="4rem 0 2rem 0">
+      <FooterWrapper>
+        <FooterGrid justify="space-between">
+          <FooterColumn id="footerLogo">
+            <FooterLogo to="/">
+              {/* <SocialIcon src="./assets/logo.png" /> */}
+              Mega Shop
+            </FooterLogo>
+            <FooterAddress></FooterAddress>
 
-          <Row align="center" margin="auto  0 0 0" gap="1rem">
-            {footerSocialData.map((social, index) => (
-              <FooterSocialIcon
-                key={index}
-                href="/"
-                target="_blank"
-                aria-label={social.name}
-              >
-                {social.icon}
-              </FooterSocialIcon>
-            ))}
-          </Row>
-        </FooterColumn>
-        {footerData.map((footerItem, index) => (
-          <FooterLinkItems key={index}>
-            <FooterLinkTitle>{footerItem.title}</FooterLinkTitle>
-            {footerItem.links.map((link, linkIndex) => (
-              <FooterLink key={linkIndex} to="/">
-                {link}
-              </FooterLink>
-            ))}
-          </FooterLinkItems>
-        ))}
-      </FooterGrid>
-      <FooterRights>Mega Shop © 2022</FooterRights>
-    </FooterWrapper>
-  </Section>
+            <Row align="center" margin="auto  0 0 0" gap="1rem">
+              {footerSocialData.map((social, index) => (
+                <FooterSocialIcon
+                  key={index}
+                  href="/"
+                  target="_blank"
+                  aria-label={social.name}
+                >
+                  {social.icon}
+                </FooterSocialIcon>
+              ))}
+            </Row>
+          </FooterColumn>
+          {footerData.map((footerItem, index) => (
+            <FooterLinkItems key={index}>
+              <FooterLinkTitle>{footerItem.title}</FooterLinkTitle>
+              {footerItem.links.map((link, linkIndex) => (
+                <FooterLink key={linkIndex} to="/">
+                  {link}
+                </FooterLink>
+              ))}
+            </FooterLinkItems>
+          ))}
+        </FooterGrid>
+        <FooterRights>Mega Shop © 2022</FooterRights>
+      </FooterWrapper>
+    </Section>
   );
 };
 
